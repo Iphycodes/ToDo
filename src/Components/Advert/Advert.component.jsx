3e56ba940e4ab2a4a3e6b6f072cbde8813c2b809
Iphycodes <ifeanyiemmanuel585@gmail.com>
2022-06-27 09:04:36 -0700

    Set the image url properly

--- src/Components/Advert/Advert.component.jsx
+++ src/Components/Advert/Advert.component.jsx
@@ -7,7 +7,8 @@ export const Advert = ({advertDatum}) => {
     
     return (
         <MainAdvertContainer>
-        <AdvertImage src={process.env.PUBLIC_URL + `/images/${advertDatum?.imageName}`} alt='pic'/>
+        {/* <AdvertImage src={process.env.PUBLIC_URL + `/images/${advertDatum?.imageName}`} alt='pic'/> */}
+        <AdvertImage src={advertDatum?.imageName} alt='pic'/>
         <AdvertHeaderContainer>
             <h2>{advertDatum?.heading}</h2>
         </AdvertHeaderContainer>
